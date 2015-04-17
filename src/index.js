bb4c7521932a2ef2ae490bedbc331ffcf2742489
Matthew Smith <mtscout6@gmail.com>
2015-04-17 15:31:56 -0600

    Initial setup to make sure css-band-aid module loads

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import bandaid from 'css-band-aid';
+
+class BandAidPlugin {
+  apply(compiler) {
+    console.log('BandAidPlugin registered');
+  }
+}
+
+export default BandAidPlugin;
---
+++ test/webpack.config.js
@@ -0,0 +1,29 @@
+require('babel/register');
+var path = require('path');
+var baseDir = path.join(__dirname, 'app');
+var BandAidPlugin = require('../src');
+
+module.exports = {
+  entry: {
+    entry1: path.join(baseDir, 'entry1.js'),
+    entry2: path.join(baseDir, 'entry2.js')
+  },
+
+  output: {
+    filename: '[name]-[hash].js',
+    path: path.join(baseDir, 'assets'),
+    publicPath: '/assets/'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.less/, loader: 'style!css!less' },
+      { test: /\.jpeg/, loader: 'file?name=[name]-[hash].[ext]' }
+    ]
+  },
+
+  plugins: [
+    new BandAidPlugin()
+  ]
+};
